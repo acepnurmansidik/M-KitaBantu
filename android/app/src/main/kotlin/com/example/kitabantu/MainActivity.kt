c4d2f414feb65621440933bb85c93352d6d69b65
acep nurman sidik <acepnurmansidik@gmail.com>
2024-07-26 14:58:56 +0700

    initialize project

---
+++ android/app/src/main/kotlin/com/example/kitabantu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kitabantu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = const Color(0xff347EFE);
+Color kSecondaryColor = const Color(0xff8AB4FE);
+Color kGreyColor = const Color(0xffBCBCBC);
+Color kBlackColor = const Color(0xff3C3C3C);
+
+TextStyle primaryTextStyle =
+    TextStyle(fontFamily: "Inter", color: kPrimaryColor);
+TextStyle greyTextStyle = TextStyle(fontFamily: "Inter", color: kGreyColor);
+TextStyle blackTextStyle = TextStyle(fontFamily: "Inter", color: kBlackColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
