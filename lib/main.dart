0d02418088bf8dc9a7be71e03999968a0d482089
acep nurman sidik <acepnurmansidik@gmail.com>
2024-08-06 22:26:15 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kitabantu/cubit/page_cubit.dart';
@@ -7,8 +8,41 @@ import 'package:kitabantu/pages/signin_page.dart';
 import 'package:kitabantu/pages/signup_page.dart';
 import 'package:kitabantu/pages/splash_page.dart';
 import 'package:kitabantu/pages/success_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> _firebaseMessagingBackgroundHandler() async {
+  // Handle a background message
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Request permission
+  FirebaseMessaging.instance.requestPermission();
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  FirebaseMessaging.instance.getInitialMessage();
+  // FirebaseMessaging.onMessage.listen((message) {
+  //   print("onMessage");
+  //   print(message.notification);
+  //   print(message.notification);
+  // });
+
+  // FirebaseMessaging.onMessageOpenedApp.listen((message) {
+  //   print("onMessageOpenedApp");
+  //   print(message);
+  // });
+
+  // FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {
+  //   // TODO: If necessary send token to application server.
+  //   print(fcmToken);
+  //   // Note: This callback is fired at each app startup and whenever a new
+  //   // token is generated.
+  // }).onError((err) {
+  //   // Error getting token.
+  // });
   runApp(const MyApp());
 }
 
